158a6f3e1d6c3d3bcfcbd2594d76e7f246c96f3e
sunbeam9 <61368572+sunbeam9@users.noreply.github.com>
2022-12-08 20:28:40 -0500

    Changes to fix some errors

--- InfiniteStamina.cs
+++ InfiniteStamina.cs
@@ -3,9 +3,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using Cysharp.Threading.Tasks;
-using OpenMod.Unturned.Plugins;
 using OpenMod.API.Plugins;
-using OpenMod.Unturned.Events
+using OpenMod.Unturned.Events;
+using OpenMod.Unturned.Players.Stats.Events;
 // For more, visit https://openmod.github.io/openmod-docs/devdoc/guides/getting-started.html
 
 [assembly: PluginMetadata("RaidCity.InfiniteStamina", DisplayName = "Raid City Infinite Stamina Plugin")]
@@ -16,7 +16,7 @@ namespace InfiniteStamina
         [EventListener(Priority = EventListenerPriority.Lowest)]
         public async Task HandleEventAsync(object sender, UnturnedPlayerEvent @event)
         {
-            if (@event.Player.PlayerLife.stamina <= 50) @event.Player.PlayerLife.serverModifyStamina(100.0);
+            if (@event.Player.Player.life.stamina <= 50) @event.Player.Player.life.serverModifyStamina(100.0);
         }
     }
 }
