7c9809944c34f52987b8260a01d145ac804a920c
sunbeam9 <61368572+sunbeam9@users.noreply.github.com>
2022-12-13 00:00:33 -0500

    Fixed plugin unloading error

--- InfiniteStamina.cs
+++ InfiniteStamina.cs
@@ -7,13 +7,32 @@ using Cysharp.Threading.Tasks;
 using OpenMod.Core.Eventing;
 using OpenMod.API.Plugins;
 using OpenMod.API.Eventing;
+using OpenMod.Unturned.Plugins;
 using OpenMod.Unturned.Events;
 using OpenMod.Unturned.Players.Stats.Events;
 
 [assembly: PluginMetadata("RaidCity.InfiniteStamina", DisplayName = "Raid City Infinite Stamina Plugin")]
 namespace InfiniteStamina
 {
-    public class InfiniteStamina : IEventListener<UnturnedPlayerStaminaUpdatedEvent>
+    public class InfiniteStaminaPlugin: OpenModUnturnedPlugin
+      {
+          public InfiniteStaminaPlugin
+          (
+              IServiceProvider serviceProvider
+          ) : base(serviceProvider)
+          {
+          }
+
+          protected override async UniTask OnLoadAsync()
+          {
+          }
+
+          protected override async UniTask OnUnloadAsync()
+          {
+          }
+      }
+
+    public class StaminaUpdatedEventListener : IEventListener<UnturnedPlayerStaminaUpdatedEvent>
     {
         [EventListener(Priority = EventListenerPriority.Lowest)]
         public Task HandleEventAsync(object? sender, UnturnedPlayerStaminaUpdatedEvent @event)
